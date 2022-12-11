4930d80015e83284523cc60d7ec20c8596b47b43
Коновалов Антон <kan2003spb@gmail.com>
2022-12-12 00:10:37 +0300

    DockStrings added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция для атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику \
 равный {5 + randint(3, 5)}')
@@ -17,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция для защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -28,6 +30,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция для спецумения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное \
 умение «Выносливость {80 + 25}»')
@@ -40,6 +43,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция для начала тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -64,6 +68,7 @@ defence — чтобы блокировать атаку противника и
 
 
 def choice_char_class() -> str:
+    """Функция для выбора класса."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
